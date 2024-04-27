064fcce8806ce525f9d6cb61032529a127ee97bf
GitHelveer <helvhelver@gmail.com>
2024-04-27 12:06:37 +0300

    the cards are update by providing a zoomeing effect and a rounded border

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,18 +7,18 @@ import salad from "./greek salad.jpg";
 function Cardcomp() {
   return (
     <div className="Cardcontainer1">
-      <Card className="cardcard" maxW="sm">
+      <Card className="cardcard" maxW="sm" transition="all 0.3s" _hover={{ boxShadow: "lg", transform: "scale(1.05)" }}>
         <CardBody className="CardBody">
           <Image src={bruchetta} alt="Green double couch with wooden legs" borderRadius="lg" />
           <Stack mt="6" spacing="4">
             <HStack justifyContent="space-between" alignItems="center">
-              <Heading className="smaller-heading" mb="0">{`Greek Salad`}</Heading>
+              <Heading className="smaller-heading" mb="0">Greek Salad</Heading>
               <Text className="Cardcontainer1text" color="blue.600" fontSize="2xl">
                 $45
               </Text>
             </HStack>
             <Text className="Cardcontainer1text" mt="4">
-             Lorem ipsum velit sed corper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus
+              Lorem ipsum velit sed corper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus
             </Text>
           </Stack>
         </CardBody>
@@ -32,18 +32,18 @@ function Cardcomp() {
 
       <div style={{ marginBottom: "20px" }} /> {/* Add space between cards */}
 
-      <Card className="cardcard2" maxW="sm">
+      <Card className="cardcard2" maxW="sm" transition="all 0.3s" _hover={{ boxShadow: "lg", transform: "scale(1.05)" }}>
         <CardBody className="CardBody">
           <Image src={salad} alt="Green double couch with wooden legs" borderRadius="lg" />
           <Stack mt="6" spacing="4">
             <HStack justifyContent="space-between" alignItems="center">
-              <Heading className="smaller-heading" mb="0">{`Brushetta`}</Heading>
+              <Heading className="smaller-heading" mb="0">Brushetta</Heading>
               <Text className="Cardcontainer1text" color="blue.600" fontSize="2xl">
                 $35
               </Text>
             </HStack>
             <Text className="Cardcontainer1text" mt="4">
-            Lorem ipsum velit sed corper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus
+              Lorem ipsum velit sed corper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus
             </Text>
           </Stack>
         </CardBody>
@@ -57,18 +57,18 @@ function Cardcomp() {
 
       <div style={{ marginBottom: "20px" }} /> {/* Add space between cards */}
 
-      <Card className="cardcard3" maxW="sm">
+      <Card className="cardcard3" maxW="sm" transition="all 0.3s" _hover={{ boxShadow: "lg", transform: "scale(1.05)" }}>
         <CardBody className="CardBody">
           <Image src={macaroni} alt="Green double couch with wooden legs" borderRadius="lg" />
           <Stack mt="6" spacing="4">
             <HStack justifyContent="space-between" alignItems="center">
-              <Heading className="smaller-heading" mb="0">{`Macaroni`}</Heading>
+              <Heading className="smaller-heading" mb="0">Macaroni</Heading>
               <Text className="Cardcontainer1text" color="blue.600" fontSize="2xl">
                 $20
               </Text>
             </HStack>
             <Text className="Cardcontainer1text" mt="4">
-            Lorem ipsum velit sed corper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus
+              Lorem ipsum velit sed corper morbi tincidunt ornare massa eget egestas purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu augue ut lectus
             </Text>
           </Stack>
         </CardBody>
